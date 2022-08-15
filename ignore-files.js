ff33409a254491f332ac10a1ca90c4a146d20b16
Lauri Tunnela <lauri@paranotek.com>
2022-08-15 17:25:19 +0300

    Use import instead of require

--- ignore-files.js
+++ ignore-files.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-var q = require('q');
-var fs = require('fs');
-var path = require('path');
-var glob = require('glob');
-var xml2js = require('xml2js');
-var del = require('del');
+import q from 'q';
+import fs from 'fs';
+import path from 'path';
+import glob from 'glob';
+import xml2js from 'xml2js';
+import {deleteSync} from 'del';
 
 module.exports = function(context) {
   var deferral = q.defer();
@@ -13,7 +13,7 @@ module.exports = function(context) {
   fs.readFile('config.xml', 'utf8', function (err, xml) {
     if (err) {
       deferral.reject('Unable to load config.xml');
-
+      
       return;
     }
     parser.parseString(xml, function (err, config) {
@@ -33,7 +33,7 @@ module.exports = function(context) {
         }
         glob(ignoreFiles[i].$.ignore, {}, function (er, files) {
           for (var i = 0, l = files.length; i < l; ++i) {
-            del.deleteSync(files[i]);
+            deleteSync(files[i]);
 
             console.log('ignore-files.js: File `' + files[i] + '` ignored.');
           }
