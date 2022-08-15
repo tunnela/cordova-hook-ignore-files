517b62a2ef1961763561a700c72f9ee8675a8e8d
Lauri Tunnela <lauri@paranotek.com>
2022-08-15 17:45:58 +0300

    Downgrade del package as ES6 import causes issues with Cordova

--- ignore-files.js
+++ ignore-files.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import q from 'q';
-import fs from 'fs';
-import path from 'path';
-import glob from 'glob';
-import xml2js from 'xml2js';
-import {deleteSync} from 'del';
+var q = require('q');
+var fs = require('fs');
+var path = require('path');
+var glob = require('glob');
+var xml2js = require('xml2js');
+var del = require('del');
 
 module.exports = function(context) {
   var deferral = q.defer();
@@ -13,7 +13,7 @@ module.exports = function(context) {
   fs.readFile('config.xml', 'utf8', function (err, xml) {
     if (err) {
       deferral.reject('Unable to load config.xml');
-      
+
       return;
     }
     parser.parseString(xml, function (err, config) {
@@ -33,7 +33,7 @@ module.exports = function(context) {
         }
         glob(ignoreFiles[i].$.ignore, {}, function (er, files) {
           for (var i = 0, l = files.length; i < l; ++i) {
-            deleteSync(files[i]);
+            del.sync(files[i]);
 
             console.log('ignore-files.js: File `' + files[i] + '` ignored.');
           }
